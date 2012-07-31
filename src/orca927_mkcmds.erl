82a03304a445f1f253d8b445c881803a4214b9a8
Ben LaRoque <benjamin.laroque@gmail.com>
2012-07-31 10:36:33 -0700

    Initializing repo.

---
+++ src/orca927_mkcmds.erl
@@ -0,0 +1,65 @@
+% generates a string to send to ORCA via telnet
+-module(orca927).
+-export([orca_make_cmd/4]).
+-export([setUpperDiscriminator/1, setLowerDiscriminator/1, setLtPreset/1, setPresetCtrlReg/0]).
+-export([clearSpectrum/0, startAcquisition/0, stopAcquisition/0, writeSpectrum/1]).
+-export([setComment/1]).
+
+orca_make_cmd(TagString, Module, Method, Argslist)->
+    {"send", lists:concat([TagString, " = [", Module," ",
+          unpack_arg_list(Method), unpack_arg_list(Argslist),"]"])}.
+
+unpack_arg_list(Arglist)->
+    (unpack_arg_list(Arglist,[])).
+
+unpack_arg_list([],Acc)->
+    lists:reverse(Acc);
+unpack_arg_list([{Name,Value}|T],Acc)->
+    Str = lists:concat([Name,":",Value," "]),
+    unpack_arg_list(T,[<<Str>> | Acc]).
+
+% ORCA methods%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    % Run condition presets
+% Set the ULD value (2^14 - 1 is effectively no limit)
+setUpperDiscriminator(Val)->
+    orca_make_cmd(somerand, "ORMCA927Model", 
+      [{setUpperDiscriminator, 0}], [{withValue, Val}]).
+% Set the LLD value (0 is effecitvely no limit)
+setLowerDiscriminator(Val)->
+    orca_make_cmd(somerand, "ORMCA927Model",
+      [{setLowerDiscriminator, 0}], [{withValue, Val}]).
+% Set the total live time for a run
+setLtPreset(Val)->
+    orca_make_cmd(somerand, "ORMCA927Model",
+      [{setLtPreset, 0}], [{withValue, Val}]).
+% Enable auto-stop at live time
+    % if PresetCtrlReg == 8 then livetime condition is enabled,
+    %I don't what other possible values exist or what state they correspond with.
+setPresetCtrlReg()->
+    orca_make_cmd(somerand, "ORMCA927Model",
+      [{setPresetCtrlReg, 0}], [{withValue, 8}]).
+
+    % Data taking control
+% Clear current spectrum
+clearSpectrum()->
+    orca_make_cmd(somerand, "ORMCA927Model",
+      [{clearSpectrum, 0}], []).
+% Start taking data
+startAcquisition()->
+    orca_make_cmd(somerand, "ORMCA927Model",
+      [{startAcquisition, 0}], []).
+% Stop taking data
+stopAcquisition()->
+    orca_make_cmd(somerand, "ORMCA927Model",
+      [{stopAcquisition, 0}], []).
+% Save current spectrum
+writeSpectrum(FileName)->
+    orca_make_cmd(somerand, "ORMCA927Model",
+      [{writeSpectrum, 0}], [{toFile,FileName}]).
+
+    % Other
+% Set the comment string
+setComment(Comment)->
+    orca_make_cmd(somerand, "ORMCA927Model",
+      [{setComment, Comment}], []).
---
+++ src/orca_interface.erl
@@ -0,0 +1,79 @@
+-module(orca_interface).
+
+-behaviour(gen_server).
+
+
+%% API
+-export([start_link/0,
+         make_connection/0,
+         make_connection/2]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, code_change/3, terminate/2]).
+
+-define(SERVER, ?MODULE).
+
+%%===============================================================
+%% API
+%%===============================================================
+
+%%---------------------------------------------------------------
+%% Function: start_link() -> {ok,Pid} | ignore | {error,Error}
+%% Description: Starts the server
+%%---------------------------------------------------------------
+start_link() ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%%---------------------------------------------------------------
+%% Function make_connection()
+%% Description: connects to the local instance of ORCA
+%%---------------------------------------------------------------
+make_connection() ->
+  make_connection("localhost", 4667).
+
+%%---------------------------------------------------------------
+%% Function make_connection(IP, Port)
+%% Description: connects to the local instance of ORCA
+%%---------------------------------------------------------------
+make_connection(IP, Port) ->
+  gen_server:cast(?SERVER, {make_connection, IP, Port}).
+
+%%===============================================================
+%% gen_server callbacks
+%%===============================================================
+init(_Args) ->
+  {ok}.
+
+%%---------------------------------------------------------------
+%% Function: handle_call
+%%---------------------------------------------------------------
+handle_call(_Request, _From, State) ->
+  Reply = ok,
+  {reply, Reply, State}.
+
+%%---------------------------------------------------------------
+%% Function: handle_cast
+%%---------------------------------------------------------------
+handle_cast({make_connection, IP, Port}, State) ->
+  {ok, Socket} = gen_tcp:connect(IP, Port, [binary, {packet, 0}]),
+  ok;
+handle_cast(_Msg, State) ->
+  {noreply, State}.
+
+%%---------------------------------------------------------------
+%% Function: handle_info
+%%---------------------------------------------------------------
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+%%---------------------------------------------------------------
+%% Function: terminate
+%%---------------------------------------------------------------
+terminate(_Reason, _State) ->
+  ok.
+
+%%---------------------------------------------------------------
+%% Function: code_change
+%%---------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
